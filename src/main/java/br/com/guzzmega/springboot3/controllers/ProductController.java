390888cb096839682f39f95d1021f9e834e605f4
Augusto Longo <guzzt.mega@gmail.com>
2023-06-15 22:02:42 -0300

    Adicionado ProductRecordDTO, ProductRepositoy e rota POST de Product

---
+++ src/main/java/br/com/guzzmega/springboot3/controllers/ProductController.java
@@ -0,0 +1,27 @@
+package br.com.guzzmega.springboot3.controllers;
+
+import br.com.guzzmega.springboot3.dtos.ProductRecordDto;
+import br.com.guzzmega.springboot3.models.ProductModel;
+import br.com.guzzmega.springboot3.repositories.ProductRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductController {
+
+	@Autowired
+	ProductRepository productRepository;
+
+	@PostMapping("/products")
+	public ResponseEntity<ProductModel> saveProduct(@RequestBody @Valid ProductRecordDto productRecordDto){
+		var productModel = new ProductModel();
+		BeanUtils.copyProperties(productRecordDto, productModel);
+		return ResponseEntity.status(HttpStatus.CREATED).body(productRepository.save(productModel));
+	}
+}
